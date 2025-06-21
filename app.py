f122b76145e17ad24a11818f74459c6b57e41376
Rishabh Jain <rishabh.z.jain@oracle.com>
2025-06-21 19:43:36 +0530

    Add deployment sanity testing

--- app.py
+++ app.py
@@ -103,5 +103,9 @@ def logout():
     session.pop('email', None)
     return redirect(url_for('home'))
 
+@app.route('/health')
+def health_check():
+    return "OK", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
